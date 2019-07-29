10ebf015ac701d2ea6b52d2d61fdde039ab93e54
Ede Meijer <ede.meijer88@gmail.com>
2019-07-29 11:21:08 +0200

    Add a navbar to navigate pages

---
+++ resources/views/layouts/timer-base.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Timer</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Tags</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+@endsection
